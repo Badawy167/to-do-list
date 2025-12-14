78e472495f1bdda714194410abc378f2fd3dea2c
Badawy167 <amostafa2392004@gamil.com>
2025-12-14 17:55:53 +0200

    up_date2

---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+use App\Models\User;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+
+class AuthController extends Controller
+{
+public function register(Request $request){
+    // تسجيل مستخدم جديد
+    $validated = $request->validate([
+        'name' => ['required','string','max:255'],
+        'email' => ['required','email','max:255','unique:users'],
+        'password' => ['required','confirmed','string','min:8'],
+    ]);
+
+    $user = User::create([
+        'name'=>$validated['name'],
+        'email'=>$validated['email'],
+        'password' => Hash::make($validated['password'])
+    ]);
+    
+        $token = $user->createToken('api_token')->plainTextToken;
+
+        return response()->json([
+            'status'=>true,
+            'message'=>[
+                'ar'=> 'تم التسجيل بنجاح',
+                'en' => 'Registered successfully'] ,
+    
+            'data'=>[
+                'id'=>$user->id,
+                'name'=>$user->name,
+                'email'=>$user->email,
+                'token'=>$token,
+            ]
+        ]);
+}
+}
